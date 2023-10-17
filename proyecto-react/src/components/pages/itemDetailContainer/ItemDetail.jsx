cf03177d38c8d4e9f5dc724bdc8b7cf50afe1719
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-10-17 18:54:24 -0300

    ajustando tamaño de imagenes

--- proyecto-react/src/components/pages/itemDetailContainer/ItemDetail.jsx
+++ proyecto-react/src/components/pages/itemDetailContainer/ItemDetail.jsx
@@ -3,10 +3,9 @@ import CounterContainer from "../../common/counter/CounterContainer";
 export const ItemDetail = ({ productSelected, onAdd }) => {
   return (
     <div>
-      {/* aca todo el detalle */}
       <h2>{productSelected.title}</h2>
-      <img src={productSelected.img} alt="" />
-      {/* Aca el contador */}
+      <img src={productSelected.img} alt="" style={{ height: "300px" }} />
+
       <CounterContainer stock={productSelected.stock} onAdd={onAdd} />
     </div>
   );
